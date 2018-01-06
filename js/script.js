91f578d845ae5b0880e3ba55af462ce63f0f1fcd
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2018-01-06 21:26:31 +0100

    Javascript login interactie toegevoegd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
 
 
-  $(function() {
-       $('#login').click(function() {
-           $('.logged-out').hide();
-           $('.login').show();
-           return false;
-       });
-   });
+function Go_to_login() {
+ document.getElementById("logged-out").style.display = "none";
+ document.getElementById("login").style.display = "block";
+}
 
 
+function Login() {
+ document.getElementById("login").style.display = "none";
+ document.getElementById("logged-in").style.display = "block";
+}
+
+
+function Logout() {
+ document.getElementById("logged-in").style.display = "none";
+ document.getElementById("logged-out").style.display = "block";
 }
