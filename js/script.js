b5f45576e6bf1cacd16eaaeb9c70f1f4d268ccb1
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2018-01-07 13:08:01 +0100

    Toegevoegd state pijltje popular sectie via js veranderen

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 
 
 function Go_to_login() {
- document.getElementById("logged-out").style.display = "none";
- document.getElementById("login").style.display = "block";
+  document.getElementById("logged-out").style.display = "none";
+  document.getElementById("login").style.display = "block";
 }
 
 
 function Login() {
- document.getElementById("login").style.display = "none";
- document.getElementById("logged-in").style.display = "block";
+  document.getElementById("login").style.display = "none";
+  document.getElementById("logged-in").style.display = "block";
 }
 
 
 function Logout() {
- document.getElementById("logged-in").style.display = "none";
- document.getElementById("logged-out").style.display = "block";
+  document.getElementById("logged-in").style.display = "none";
+  document.getElementById("logged-out").style.display = "block";
+}
+
+function Popular_switch() {
+  document.getElementById("more-arrow").style.background = "url('images/carousel-pijltjes.png') 27px 0px";
 }
